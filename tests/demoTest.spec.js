7af6367160e14488b7e7f7ba414857c5853eddad
sssm777 <sssm777@github.com>
2023-03-22 14:53:03 +0530

    removed commented Lines

--- tests/demoTest.spec.js
+++ tests/demoTest.spec.js
@@ -5,11 +5,4 @@ test('test', async ({ page }) => {
     const login= new LoginPage(page);
   await  login.gotoLoginPage();
    await login.login("tomsmith","SuperSecretPassword!");
-//   await page.goto('https://the-internet.herokuapp.com/login');
-//   await page.getByLabel('Username').click();
-//   await page.getByLabel('Username').fill('tomsmith');
-//   await page.getByLabel('Password').click();
-//   await page.getByLabel('Password').fill('SuperSecretPassword!');
-//   await page.getByRole('button', { name: ' Login' }).click();
-//   await page.getByRole('link', { name: 'Logout' }).click();
 });
